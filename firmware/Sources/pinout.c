adfd2afd37f0bcfff5b56801b71b86a323345027
Marco Giammarini <m.giammarini@warcomeb.it>
2013-12-27 18:46:01 +0100

    Added files for manage ARM Matrix pinout.

---
+++ firmware/Sources/pinout.c
@@ -0,0 +1,46 @@
+/******************************************************************************
+ * Copyright (C) 2013-2014 Marco Giammarini
+ * 
+ * Author(s):
+ *  Marco Giammarini <m.giammarini@warcomeb.it>
+ *  
+ * Project: TatsumakiBot
+ * Package: -
+ * Version: 0.0
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>
+ ******************************************************************************/
+
+/**
+ * @file Project_Headers/pinout.c
+ * @author Marco Giammarini <m.giammarini@warcomeb.it>
+ * @brief ARM matrix pinout definitions. 
+ */
+
+#include "pinout.h"
+#include "board.h"
+
+void setPinout (void)
+{
+#if (PCB_VERSION == 0)
+    
+#endif
+}
+
+void initGpio (void)
+{    
+#if (PCB_VERSION == 0)
+    
+#endif
+}

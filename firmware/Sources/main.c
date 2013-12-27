2f9c5ee266a7e22b27b6470c1625be33e5c1df8b
Marco Giammarini <m.giammarini@warcomeb.it>
2013-12-27 18:55:39 +0100

    Added some initialization into main file.

--- firmware/Sources/main.c
+++ firmware/Sources/main.c
@@ -1,26 +1,68 @@
-/*
- * main implementation: use this 'C' sample to create your own application
+/******************************************************************************
+ * Copyright (C) 2013-2014 Marco Giammarini
+ * 
+ * Author(s):
+ *	Marco Giammarini <m.giammarini@warcomeb.it>
+ *	
+ * Project: TatsumakiBot
+ * Package: -
+ * Version: 0.0
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
  *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>
+ ******************************************************************************/
+
+/**
+ * @file Sources/main.c
+ * @author Marco Giammarini <m.giammarini@warcomeb.it>
+ * @brief Main file of the application. 
  */
 
+/**
+ * @mainpage TatsumakiBot - OHIBoard based mini-sumo robot
+ * 
+ * @author Marco Giammarini <m.giammarini@warcomeb.it>
+ */
 
+/* OHIBoard Library (LGPL) */
+#include "libohiboard.h"
 
-
-
-#include "derivative.h" /* include peripheral declarations */
-
-
+#include "board.h"
+#include "pinout.h"
 
 int main(void)
 {
-	int counter = 0;
-	
-	
-	
-	
-	for(;;) {	   
-	   	counter++;
-	}
-	
-	return 0;
+int counter = 0;
+
+    /* Enable clock gate for ports to enable pin routing */
+    SIM_SCGC5 |= (
+            SIM_SCGC5_PORTA_MASK | 
+            SIM_SCGC5_PORTB_MASK | 
+            SIM_SCGC5_PORTC_MASK | 
+            SIM_SCGC5_PORTD_MASK | 
+            SIM_SCGC5_PORTE_MASK);
+    
+    /* Clock initialization */
+    System_initClock();
+    
+    setPinout();
+    initGpio();
+
+
+
+for(;;) {	   
+counter++;
+}
+
+return 0;
 }

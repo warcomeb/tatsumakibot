6e1aa40695941c264ad33992ba8e81c7f0207d16
Marco Giammarini <m.giammarini@warcomeb.it>
2014-01-18 00:14:06 +0100

    Started project of desktop interface to control robot.

---
+++ software/desktop/TatsumakiDesktop/mainwindow.cpp
@@ -0,0 +1,48 @@
+/******************************************************************************
+ * TatsumakiBot Desktop Control Interface
+ * Copyright (C) 2014 Marco Giammarini
+ *
+ * Author(s):
+ *  Marco Giammarini <m.giammarini@warcomeb.it>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>
+ ******************************************************************************/
+
+/**
+ * @file mainwindow.cpp
+ * @author Marco Giammarini <m.giammarini@warcomeb.it>
+ * @brief Main window class methods implementations.
+ */
+
+/**
+ * @mainpage TatsumakiBot Desktop Control Interface
+ *
+ *
+ * @author Marco Giammarini <m.giammarini@warcomeb.it>
+ */
+
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
---
+++ software/desktop/TatsumakiDesktop/mainwindow.h
@@ -0,0 +1,49 @@
+/******************************************************************************
+ * TatsumakiBot Desktop Control Interface
+ * Copyright (C) 2014 Marco Giammarini
+ *
+ * Author(s):
+ *  Marco Giammarini <m.giammarini@warcomeb.it>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>
+ ******************************************************************************/
+
+/**
+ * @file mainwindow.cpp
+ * @author Marco Giammarini <m.giammarini@warcomeb.it>
+ * @brief Main window class definitions.
+ */
+
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+
+namespace Ui {
+class MainWindow;
+}
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit MainWindow(QWidget *parent = 0);
+    ~MainWindow();
+
+private:
+    Ui::MainWindow *ui;
+};
+
+#endif // MAINWINDOW_H
